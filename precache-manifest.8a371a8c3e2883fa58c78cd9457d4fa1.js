23d412c5417ba61584c945946c88280208a9d7ab
Ishtmeet Rissam <ishtmeet@decodershub.com>
2019-03-16 21:24:32 +0530

    finali

---
+++ precache-manifest.8a371a8c3e2883fa58c78cd9457d4fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ff4733b366b5fa329273ef06eb54b0f",
+    "url": "/static/media/home-bg.6ff4733b.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c5ffb8b78e2ac9be10a9",
+    "url": "/static/js/main.b06fa3c8.chunk.js"
+  },
+  {
+    "revision": "f064eee891b6842871e4",
+    "url": "/static/js/2.50a9cdcb.chunk.js"
+  },
+  {
+    "revision": "c5ffb8b78e2ac9be10a9",
+    "url": "/static/css/main.75b267a5.chunk.css"
+  },
+  {
+    "revision": "c9073216f45a33a0b5e22d41d8227e5e",
+    "url": "/index.html"
+  }
+];
